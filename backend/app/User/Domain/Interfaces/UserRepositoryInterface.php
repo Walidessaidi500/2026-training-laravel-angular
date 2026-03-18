52bbc39db59754c24c877077e6cb0000229bbe51
Walidessaidi500 <90364671+Walidessaidi500@users.noreply.github.com>
2026-03-18 17:18:09 +0100

    Añadida la capa de Dominio y Aplicación para el login de usuarios - Añadido el método verify() a PasswordHasherInterface - Creada la interfaz TokenGeneratorInterface - Añadido el método findByEmail() a UserRepositoryInterface - Creado el caso de uso LoginUser y su respuesta (LoginUserResponse)

--- backend/app/User/Domain/Interfaces/UserRepositoryInterface.php
+++ backend/app/User/Domain/Interfaces/UserRepositoryInterface.php
@@ -9,4 +9,7 @@ interface UserRepositoryInterface
     public function save(User $user): void;
 
     public function findById(string $id): ?User;
+
+    /*Esto lo que hace es buscar un usuario por su correo electronico*/
+    public function findByEmail(Email $email): ?User;
 }
